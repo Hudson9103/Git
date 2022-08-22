e97728feb8628d15613db4246eeb606045ade351
Hudson9103 <hudson.ulisses@outlook.com>
2022-08-22 18:54:48 -0300

    Atualização

---
+++ Projetos/Desenvolvimento WEB/Curso - PHP/exercicio.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/CSS/style.css">
+    <link rel="stylesheet" href="assets/CSS/exercicio.css">
+    <title>Exercício</title>
+</head>
+<body class="exercicio">
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Visualização do ExercÍcios</h2>
+    </header>
+    <nav class="navegacao">
+        <a href="#" class="verde">Sem formatação</a>
+        <a href="index.php" class="vermelho">Voltar</a>
+    </nav>
+    <main class="principal">
+        <div class="conteudo">
+           
+            
+        </div>
+    </main>
+    <footer class="rodape">
+        COD3R & ALUNOS <?= date('Y'); ?>
+    </footer>
+</body>
+</html>
